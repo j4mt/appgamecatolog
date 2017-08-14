8b7644ecfdcbe64ca469e057da47c1dde53c54d6
John O'Rourke <John.ORourke@sqs.com>
2017-08-14 16:24:45 +0100

    Initial commit

---
+++ src/main/java/com/j4mt/app/gamecatolog/config/MvcConfig.java
@@ -0,0 +1,17 @@
+package com.j4mt.app.gamecatolog.config;
+
+import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+
+@Configuration
+public class MvcConfig
+        //extends WebMvcAutoConfiguration.WebMvcAutoConfigurationAdapter
+        {
+
+//    @Override
+//    public void addViewControllers(ViewControllerRegistry registry) {
+//        registry.addViewController("/").setViewName("home");
+//        registry.addViewController("/login").setViewName("login");
+//    }
+}
