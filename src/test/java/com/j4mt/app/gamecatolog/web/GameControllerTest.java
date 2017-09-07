8fec4ef110dfbb6fa5682d7e0a4e569db760d1f3
j4mt <john.orourke.1985@gmail.com>
2017-09-07 22:34:45 +0100

    added game controller test and edited git ignore

--- src/test/java/com/j4mt/app/gamecatolog/web/GameControllerTest.java
+++ src/test/java/com/j4mt/app/gamecatolog/web/GameControllerTest.java
@@ -1,4 +1,42 @@
 package com.j4mt.app.gamecatolog.web;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class GameControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+
+    @Test
+    public void getGame() throws Exception {
+
+        this.mockMvc.perform(get("/game/1")
+                .accept(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json;charset=UTF-8"))
+                .andExpect(jsonPath("$.name").value("Sonic"));
+    }
+
 }
